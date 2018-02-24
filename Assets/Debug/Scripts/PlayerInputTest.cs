787140210d01e178e60561f20b7ad6ca2384c18d
eraz <eraz@umich.edu>
2018-02-23 21:10:34 -0500

    Write PlayerInputManager (with debug assets)

---
+++ Assets/Debug/Scripts/PlayerInputTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+[RequireComponent(typeof(PlayerInputManager))]
+public class PlayerInputTest : MonoBehaviour {
+    PlayerInputManager pim;
+
+    InputDevice device;
+
+    void Awake() {
+        pim = GetComponent<PlayerInputManager>();
+    }
+
+    void Update() {
+        if (device == null) {
+            device = pim.GetInputDevice(RemoveDevice);
+
+            if (device != null) {
+                Debug.LogFormat(this, "{0}: Device {1} acquired!", name, device.SortOrder);
+            }
+
+            return;
+        }
+
+        if (device.Action1.WasPressed) Bark();
+    }
+
+    void RemoveDevice() {
+        Debug.LogFormat(this, "{0}: Device {1} removed!", name, device.SortOrder);
+        device = null;
+    }
+
+    void Bark() {
+        Debug.LogFormat(this, "{0}: BARK by {1}!", name, device.SortOrder);
+    }
+}
---
+++ Assets/Scripts/PlayerInputManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using InControl;
+
+// InputManager
+public class PlayerInputManager : MonoBehaviour {
+    public static int maxPlayers { get { return 4; } }
+
+    // Keep track of input devices and their assignments.
+    Dictionary<InputDevice, bool> devices = new Dictionary<InputDevice, bool>();
+
+    // Registered actions to invoke on detach for each device.
+    Dictionary<InputDevice, Action> actions = new Dictionary<InputDevice, Action>();
+
+    void Start() {
+        // Assign currently attached devices in order.
+        foreach (var device in InputManager.Devices) {
+            if (devices.Count >= 4) {
+                Debug.LogFormat(this, "How are there more than 4 controllers?!");
+                break;
+            }
+
+            Debug.LogFormat("{0}: Adding existing device {1} to list!", name, device.SortOrder);
+
+            devices.Add(device, false);
+        }
+
+        // New device attached.
+        InputManager.OnDeviceAttached += (device) => {
+            Debug.LogFormat(this, "{0}: New device detected! Adding {1} to list.", name, device.SortOrder);
+            devices.Add(device, false);
+        };
+
+        // Device detached.
+        InputManager.OnDeviceDetached += (device) => {
+            Debug.LogFormat(this, "{0}: Removing device {1} from list.", name, device.SortOrder);
+            devices.Remove(device);
+        };
+    }
+
+    // Returns the next available input device; null if none is available.
+    // Registers the action to be called when a device is detached (one-time only).
+    public InputDevice GetInputDevice(Action action) {
+        var e = devices.FirstOrDefault(ee => ee.Value == false);
+
+        var device = e.Key;
+
+        if (device == null) return null;
+
+        devices[device] = true;
+        actions.Add(device, action);
+        InputManager.OnDeviceDetached += (d) => {
+            if (d == device) {
+                actions[d]();
+                actions.Remove(d);
+            }
+        };
+
+        return device;
+    }
+}
