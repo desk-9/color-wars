9f92c7f14881fac1a63b64527051827fe5a4e9f7
kjvic <kjvic@umich.edu>
2018-02-23 21:11:59 -0500

    Add scene LAB_ball, ball class

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // A player is allowed to "possess" the ball
+    GameObject currentOwner = null;
+    
+    public void SetOwner(GameObject newOwner) {
+        currentOwner = newOwner;
+    }
+
+    public void ClearOwner() {
+        currentOwner = null;
+    }
+    
+}
