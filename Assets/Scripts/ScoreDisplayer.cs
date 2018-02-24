e0ff7ef8e40d0d5a79b7863637ece5d35fc19f1d
kjvic <kjvic@umich.edu>
2018-02-23 19:45:02 -0500

    Add scoreboard & team component

---
+++ Assets/Scripts/ScoreDisplayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplayer : MonoBehaviour {
+
+    public Text ScoreDisplay;
+    public SortedDictionary<int, int> scores = new SortedDictionary<int, int>();
+
+    public static ScoreDisplayer instance;
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start() {
+        StartCoroutine(InitScores());
+    }
+
+    public void RegisterTeam(int teamNumber) {
+        if (scores.ContainsKey(teamNumber)) {
+            Debug.LogWarning("Tried to register team " + teamNumber.ToString() + " twice!");
+            return;
+        }
+        scores[teamNumber] = 0;
+    }
+
+    IEnumerator InitScores() {
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForFixedUpdate();
+        UpdateScores();
+    }
+
+    void UpdateScores() {
+        string scoreText = "";
+        foreach (var score in scores) {
+            scoreText += "Team " + score.Key.ToString() + ": " + score.Value.ToString() + "\n";
+        }
+        ScoreDisplay.text = scoreText;
+    }
+
+    public void SetScore(int teamNumber, int newScore) {
+        scores[teamNumber] = newScore;
+        UpdateScores();
+    }
+    
+
+}
---
+++ Assets/Scripts/TeamManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeamManager : MonoBehaviour {
+
+    public int teamNumber;
+
+    int score = 0;
+
+	void Start () {
+        ScoreDisplayer.instance.RegisterTeam(teamNumber);
+	}
+
+    public void ResetScore() {
+        score = 0;
+        ScoreDisplayer.instance.SetScore(teamNumber, score);
+    }
+    
+    public void IncrementScore() {
+        ++score;
+        ScoreDisplayer.instance.SetScore(teamNumber, score);
+    }
+    
+}
