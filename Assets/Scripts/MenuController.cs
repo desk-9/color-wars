dbff67b80413870708ce5ab6fac1fae88f37bbee
eraz <eraz@umich.edu>
2018-02-26 20:50:10 -0500

    Fix bug with scene reset and missing PauseMenu object

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,45 +4,16 @@ using UnityEngine;
 
 using IC = InControl;
 
+[RequireComponent(typeof(GameModel))]
 public class MenuController : MonoBehaviour {
     public IC.InputControlType StartButton = IC.InputControlType.Start;
     public IC.InputControlType ResetButton = IC.InputControlType.DPadDown;
+    public PauseMenu pauseMenu = null;
 
     GameModel gameModel = null;
-    PauseMenu pauseMenu = null;
 
     void Start() {
-        var menu = GameObject.Find("PauseMenu");
-
-        if (!menu) {
-            Debug.LogWarningFormat(this, "{0}: Could not find 'PauseMenu' object! Disabling component.", name);
-            enabled = false;
-            return;
-        }
-
-        pauseMenu = menu.GetComponent<PauseMenu>();
-
-        if (!pauseMenu) {
-            Debug.LogErrorFormat(this, "{0}: PauseMenu is missing the PauseMenu component! Disabling component.", name);
-            enabled = false;
-            return;
-        }
-
-        var game = GameObject.Find("GameModel");
-
-        if (!game) {
-            Debug.LogWarningFormat(this, "{0}: Could not find 'GameModel' object! Disabling component.", name);
-            enabled = false;
-            return;
-        }
-
-        gameModel = game.GetComponent<GameModel>();
-
-        if (!gameModel) {
-            Debug.LogErrorFormat(this, "{0}: GameModel is missing the GameModel component! Disabling component.", name);
-            enabled = false;
-            return;
-        }
+        gameModel = GetComponent<GameModel>();
     }
 
     void Update () {
