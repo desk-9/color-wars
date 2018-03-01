6380da9188691c52215b0c47b821c7fdb364a152
Spruce Bondera <sprucebondera@gmail.com>
2018-02-28 22:23:30 -0600

    Add goal with scoring on player enter

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Goal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// TODO: Change this to take some sort of ball component
+	void ScoreGoal(GameObject thing) {
+		GameModel.instance.teams.First().IncrementScore();
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider) {
+		// TODO: Change this to check for ball
+		if (collider.GetComponent<PlayerMovement>()) {
+			ScoreGoal(collider.gameObject);
+		}
+	}
+	
+}
