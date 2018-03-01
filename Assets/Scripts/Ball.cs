d44f18c3aac221074e22d7947c4e5f07852bfc18
kjvic <kjvic@umich.edu>
2018-02-28 21:55:34 -0600

    Add BallCarrier component

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 
     // A player is allowed to "possess" the ball
-    public Player owner = null;
+    public BallCarrier owner = null;
 
 	public void OnCollisionEnter2D(Collision2D collision) {
 		var collided = collision.gameObject;
-		// The assumption here is that a gameObject will have a Player component
-		// iff the gameObject can own the ball
-		var player = collided.GetComponent<Player>();
+		// The assumption here is that a gameObject will have a BallCarrier component
+		// iff the gameObject can own/carry the ball
+		var player = collided.GetComponent<BallCarrier>();
 		if (player != null) {
 			owner = player;
+			player.NotifyCarry(this.gameObject);
 		}
 	}
     
