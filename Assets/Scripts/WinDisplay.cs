53ce31f78ebff71463f35ad1092e566ef16bbcad
Spruce Bondera <sprucebondera@gmail.com>
2018-03-05 00:07:06 -0500

    Fix spacing

--- Assets/Scripts/WinDisplay.cs
+++ Assets/Scripts/WinDisplay.cs
@@ -5,20 +5,20 @@ using UnityEngine.UI;
 using UtilityExtensions;
 
 public class WinDisplay : MonoBehaviour {
-	Text winnerText;
-	Text restartTime;
-	void Awake () {
-		winnerText = transform.FindComponent<Text>("WinnerText");
-		restartTime = transform.FindComponent<Text>("RestartText");
-	}
+    Text winnerText;
+    Text restartTime;
+    void Awake () {
+        winnerText = transform.FindComponent<Text>("WinnerText");
+        restartTime = transform.FindComponent<Text>("RestartText");
+    }
 
-	public void SetWinner(TeamManager winner) {
-		winnerText.text = string.Format("Team {0} won with {1} points",
-										winner.teamNumber, winner.score);
-		winnerText.color = winner.teamColor;
-	}
+    public void SetWinner(TeamManager winner) {
+        winnerText.text = string.Format("Team {0} won with {1} points",
+                                        winner.teamNumber, winner.score);
+        winnerText.color = winner.teamColor;
+    }
 
-	public void SetRestartTime(float time) {
-		restartTime.text = string.Format("Restarting in {0:n0}...", time);
-	}
+    public void SetRestartTime(float time) {
+        restartTime.text = string.Format("Restarting in {0:n0}...", time);
+    }
 }
