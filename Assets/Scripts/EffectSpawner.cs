819a3ed669edd39f0e99c72a7e40b1920739ef8d
Spruce Bondera <sprucebondera@gmail.com>
2018-03-09 00:54:13 -0500

    Change flag names

--- Assets/Scripts/EffectSpawner.cs
+++ Assets/Scripts/EffectSpawner.cs
@@ -5,8 +5,8 @@ public class EffectSpawner : MonoBehaviour {
     public GameObject effectPrefab;
     GameObject currentEffect;
     public State triggerState;
-    public bool destroyEffect = true;
-    public bool parentEffect = true;
+    public bool destroyEffectOnExit = true;
+    public bool parentEffectToPlayer = true;
     PlayerStateManager stateManager;
 
     void Start() {
@@ -16,14 +16,14 @@ public class EffectSpawner : MonoBehaviour {
 
     void EffectToggle(bool effectStarting) {
         if (effectStarting) {
-            if (parentEffect) {
+            if (parentEffectToPlayer) {
                 currentEffect = Instantiate(effectPrefab, transform.position,
                                             Quaternion.identity, transform);
             } else {
                 currentEffect = Instantiate(effectPrefab, transform.position,
                                             Quaternion.identity);
             }
-        } else if (!effectStarting && destroyEffect) {
+        } else if (!effectStarting && destroyEffectOnExit) {
             Destroy(currentEffect);
         }
     }
