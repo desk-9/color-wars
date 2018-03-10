deb8696b6f415b8504e02878e07afe0f0a921e07
kjvic <kjvic@umich.edu>
2018-03-09 19:41:41 -0500

    Add charged shot

--- Assets/Scripts/ShootBallMechanic.cs
+++ Assets/Scripts/ShootBallMechanic.cs
@@ -6,48 +6,105 @@ using IC = InControl;
 
 public class ShootBallMechanic : MonoBehaviour {
 
-    public float shotSpeed;
-    public float forcedShotTime;
     public IC.InputControlType shootButton = IC.InputControlType.Action1;
+    public float forcedShotTime;
+    public float baseShotSpeed = 1.0f;
+    public float chargeRate    = 1.0f;
+    public GameObject          chargeEffect;
 
-    PlayerMovement playerMovement;
-    IC.InputDevice inputDevice;
-    Coroutine shootTimer;
-    BallCarrier ballCarrier;
+    PlayerMovement     playerMovement;
     PlayerStateManager stateManager;
+    Rigidbody2D        rb;
+    IC.InputDevice     inputDevice;
+    Coroutine          shootTimer;
+    Coroutine          chargeShot;
+    BallCarrier        ballCarrier;
+    GameObject         effect;
+    bool canShoot = false;
+
+    float shotSpeed = 1.0f;
+    float elapsedTime = 0.0f;
 
     IEnumerator ShootTimer() {
-        float elapsedTime = 0f;
+        Debug.Log("ShootTimer!");
+        elapsedTime = 0.0f;
         while (elapsedTime < forcedShotTime) {
-            if (inputDevice != null) {
-                if (inputDevice.GetControl(shootButton).WasPressed){
-                    break;
-                }
-            }
             elapsedTime += Time.deltaTime;
+            if (inputDevice.GetControl(shootButton).WasPressed) {
+                StartChargeShot();
+            }
             yield return null;
         }
+
+        shootTimer = null;
         Shoot();
+        yield break;
+    }
+
+
+    void StartChargeShot() {
+        Debug.Log("StartChargeShot!");
+        chargeShot = StartCoroutine(ChargeShot());
+        effect = Instantiate(chargeEffect, transform.position, Quaternion.identity, transform);
+    }
+    IEnumerator ChargeShot() {
+        Debug.Log("ChargeShot!");
+        while (elapsedTime < forcedShotTime) {
+            shotSpeed += chargeRate * Time.deltaTime;
+            if (inputDevice.GetControl(shootButton).WasReleased) {
+                Debug.Log("Trigger released => Shoot!");
+                chargeShot = null;
+                Shoot();
+                yield break;
+            }
+            yield return null;
+        }
     }
 
     void Shoot() {
+        if (!canShoot) {
+            Debug.LogWarning("Can't shoot!");
+            return;
+        }
         var ball = ballCarrier.ball;
-        ballCarrier.DropBall();
+        Debug.Assert(ball != null);
         var shotDirection = ball.transform.position - transform.position;
         var ballRigidBody = ball.EnsureComponent<Rigidbody2D>();
         ballRigidBody.velocity = shotDirection.normalized * shotSpeed;
         stateManager.CurrentStateHasFinished();
+        StopChargeShot();
+    }
+
+    void StopChargeShot() {
+        Debug.Log("StopChargeShot!");
+        // Release player from position lock.
+        rb.constraints = RigidbodyConstraints2D.None;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        
+        if (shootTimer != null) {
+            StopCoroutine(shootTimer);
+            shootTimer = null;
+        }
+        if (effect != null) {
+            Destroy(effect);
+        }
+        if (chargeShot != null) {
+            StopCoroutine(chargeShot);
+            chargeShot = null;
+        }
     }
 
     void Update() {
         if (inputDevice == null) {
             inputDevice = playerMovement.GetInputDevice();
+            return;
         }
     }
 
     void Start() {
         playerMovement = this.EnsureComponent<PlayerMovement>();
-        ballCarrier = this.EnsureComponent<BallCarrier>();
+        ballCarrier  = this.EnsureComponent<BallCarrier>();
+        rb           =  this.EnsureComponent<Rigidbody2D>();
         stateManager =  this.EnsureComponent<PlayerStateManager>();
         stateManager.CallOnStateEnter(
             State.Posession, () => shootTimer = StartCoroutine(ShootTimer()));
