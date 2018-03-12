337ece19c709c02a804b98c96cd58bc9875313a1
dkonik <dkonik@umich.edu>
2018-03-11 20:59:40 -0400

    Added goal switch timer

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UtilityExtensions;
+using UnityEngine.UI;
 
 public class Goal : MonoBehaviour {
 
     public TeamManager currentTeam;
     public float goalSwitchInterval = 10;
+    public int goalSwitchNotificationLength = 3;
+    public AudioClip goalSwitchWarningAudio;
+    public AudioClip goalSwitchAudio;
 
     IntCallback nextTeamIndex;
     new SpriteRenderer renderer;
+    Text goalSwitchText;
+    AudioSource audio;
 
     void Awake() {
         renderer = GetComponent<SpriteRenderer>();
@@ -18,14 +24,34 @@ public class Goal : MonoBehaviour {
 
     void Start () {
         nextTeamIndex = Utility.ModCycle(0, GameModel.instance.teams.Length);
+        goalSwitchText = GetComponentInChildren<Text>();
+        audio = GetComponent<AudioSource>();
         SwitchToNextTeam();
         StartCoroutine(TeamSwitching());
     }
 
+    void PlayAudioClip(AudioClip play) {
+        audio.clip = play;
+        audio.Play();
+    }
+
+    void SetNotificationText(string to) {
+        if (!goalSwitchText.enabled) {
+            goalSwitchText.enabled = true;
+        }
+        goalSwitchText.text = to;
+        PlayAudioClip(goalSwitchWarningAudio);
+    }
+
     IEnumerator TeamSwitching() {
         while (true) {
-            yield return new WaitForSeconds(goalSwitchInterval);
-            SwitchToNextTeam();
+            yield return new WaitForSeconds(goalSwitchInterval - goalSwitchNotificationLength);
+            for (int i = goalSwitchNotificationLength; i > 0; --i) {
+                SetNotificationText(i.ToString());
+                yield return new WaitForSeconds(1);
+            }
+            goalSwitchText.enabled = false;
+            SwitchToNextTeam(true);
         }
     }
 
@@ -33,7 +59,10 @@ public class Goal : MonoBehaviour {
         return GameModel.instance.teams[nextTeamIndex()];
     }
 
-    void SwitchToNextTeam() {
+    void SwitchToNextTeam(bool playSound = false) {
+        if (playSound) {
+            PlayAudioClip(goalSwitchAudio);
+        }
         currentTeam = GetNextTeam();
         if (renderer != null) {
             renderer.color = currentTeam.teamColor;
