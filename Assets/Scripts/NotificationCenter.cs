b9455e8744411d68791180219bcae3f23508b246
Spruce Bondera <sprucebondera@gmail.com>
2018-03-14 21:22:35 -0400

    Add message-enum-based system

--- Assets/Scripts/NotificationCenter.cs
+++ Assets/Scripts/NotificationCenter.cs
@@ -12,6 +12,11 @@ public delegate void PlayerTransitionCallback(Player player, State start, State 
 // Replacement for EventHandler without the EventArgs
 public delegate void EventCallback(object sender);
 
+
+public enum Message {
+
+};
+
 public class NotificationCenter {
 
     // PlayerState addons
@@ -20,6 +25,17 @@ public class NotificationCenter {
     SortedDictionary<State, PlayerCallback> on_any_player_end_subscribers =
         new SortedDictionary<State, PlayerCallback>();
     PlayerTransitionCallback on_any_change_subscribers = delegate{};
+
+    public NotificationCenter() {
+        foreach (var state in (State[]) System.Enum.GetValues(typeof(State))) {
+            on_any_player_start_subscribers[state] = delegate{};
+            on_any_player_end_subscribers[state] = delegate{};
+        }
+
+        foreach (var event_type in (Message[]) System.Enum.GetValues(typeof(Message))) {
+            on_message[event_type] = delegate{};
+        }
+    }
 
     public void HookUpPlayer(PlayerStateManager player) {
         var playerComponent = player.GetComponent<Player>();
@@ -45,11 +61,35 @@ public class NotificationCenter {
         on_any_change_subscribers += callback;
     }
 
-    // String-based event system
+    // Enum-based callback system
+    //
+    // Useful for publishing "system-wide" events that are meant to stick around
+    // for a while/be maintainable. You must add a new event name to the Message
+    // enum, then ensure something is calling NotifyMessage appropriately.
+    SortedDictionary<Message, EventCallback> on_message =
+        new SortedDictionary<Message, EventCallback>();
 
+    public void CallOnMessageWithSender(Message event_type, EventCallback callback) {
+        on_message[event_type] += callback;
+    }
+
+    public void CallOnMessage(Message event_type, Callback callback) {
+        on_message[event_type] += (object o) => callback();
+    }
+
+    public void NotifyMessage(Message event_type, object sender) {
+        on_message[event_type](sender);
+    }
+
+    // String-based event system
+    //
     // Useful for hacky/quick setup of simple events without writing extra code,
     // but isn't typesafe/you're gonna have to assume whatever implicit
-    // contracts you have are kept
+    // contracts you have are kept. Good for dynamically-created event systems,
+    // "private" systems (communicate some arbitrary string), or one-off cases
+    // where you're really pretty sure only one pair of producer/consumer needs
+    // this an a whole new enum value would be over the top. Think carefully
+    // before using this system.
     SortedDictionary<string, EventCallback> string_events =
         new SortedDictionary<string, EventCallback>();
 
