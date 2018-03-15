38ffe5a708a893109118533a8bcd82c354b7f402
Spruce Bondera <sprucebondera@gmail.com>
2018-03-14 21:05:25 -0400

    Untabify

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class AudioManager {
-	public static void Play(string name, float volume = 1) {
-		AudioClip asset = (AudioClip) Resources.Load("Sounds/" + name, typeof(AudioClip));
-		if (asset != null) {
-			AudioSource.PlayClipAtPoint(asset, Camera.main.transform.position, volume);
-		}
-	}
+    public static void Play(string name, float volume = 1) {
+        AudioClip asset = (AudioClip) Resources.Load("Sounds/" + name, typeof(AudioClip));
+        if (asset != null) {
+            AudioSource.PlayClipAtPoint(asset, Camera.main.transform.position, volume);
+        }
+    }
 }
+
