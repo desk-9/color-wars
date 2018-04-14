66c45c8ae62aa20b3b3606ac42814a85a457ef6d
dkonik <dkonik@umich.edu>
2018-04-14 03:02:40 -0400

    Added bounding corners and recording icon

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UtilityExtensions;
+
+public class Blink : MonoBehaviour {
+
+    new SpriteRenderer renderer;
+
+    // Use this for initialization
+    void Start () {
+        renderer = this.EnsureComponent<SpriteRenderer>();
+        StartCoroutine(BlinkRenderer());
+    }
+
+    IEnumerator BlinkRenderer() {
+        while (true) {
+            renderer.enabled = !renderer.enabled;
+            yield return new WaitForSecondsRealtime(.5f);
+        }
+    }
+}
